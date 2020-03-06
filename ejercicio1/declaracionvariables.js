43ff16617d4148bdf89fa8675541aa617d630656
Sebas <sebaszapata20@gmail.com>
2020-03-06 18:00:57 -0500

    Agregado git para ignorar node_modules

---
+++ ejercicio1/declaracionvariables.js
@@ -0,0 +1,20 @@
+
+/*let nombre = "juan";
+let nombre = "camilo";
+console.log(nombre);*/
+
+let costo = 5;
+let comida ="";
+if(costo > 3){
+    comida = "galletas";
+    if(costo > 4){
+        let paseo ="jardin";
+    }
+}
+console.log("La comida: " + comida);
+
+for(var i=0; i<5; i++){
+
+}
+
+console.log(i);
